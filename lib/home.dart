833d6567a00a502e5489f7495a9489e8b4073be8
Ruben Manuel <rubenadilsonsapalomanuel@gmail.com>
2022-11-07 12:32:02 +0100

    Movimentando a camera no mapa

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -15,8 +14,22 @@ class _HomeState extends State<Home> {
 
 _onMapCreated(GoogleMapController googleMapController){
    _controller.complete(googleMapController);
+}
 
-   _controller.future;
+//metodo para movimentar a camera com o seu angulo, rotacao e zom
+_movimentarCamera() async {
+ GoogleMapController googleMapController = await _controller.future;
+ googleMapController.animateCamera(
+    CameraUpdate.newCameraPosition(
+      CameraPosition(
+        target: LatLng(-8.822453, 13.234531),
+        zoom: 16,
+        tilt: 0,
+        bearing: 350
+      ),
+    )
+  );
+
 }
 
   @override
@@ -25,6 +38,11 @@ _onMapCreated(GoogleMapController googleMapController){
       appBar: AppBar(
         title: Text("Mapa & Geolocalização"),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _movimentarCamera(),
+        child: Icon(Icons.done_all),
+        ),
       body: Container(
         child: GoogleMap(
         mapType: MapType.normal, 
